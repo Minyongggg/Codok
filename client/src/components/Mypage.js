2079b28cb3960a92ad43816e2d9f2800402672c5
Minyongggg <54466093+Minyongggg@users.noreply.github.com>
2021-11-11 16:54:27 +0900

    feat: chatlist, chatroom, chatbar

--- client/src/components/Mypage.js
+++ client/src/components/Mypage.js
@@ -2,16 +2,6 @@ import { React, useEffect } from "react";
 import axios from "axios";
 
 function Mypage() {
-  useEffect(() => {
-    axios({
-      method: "post",
-      url: "http://localhost:8000/api/lectures/courseIdList",
-      data: { courseIdList: ["COSE222-01", "COSE341-01", "COSE342-01"] },
-      withCredentials: true,
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  });
   return <div>mypage</div>;
 }
 
---
+++ client/src/components/chat/Chatlist/Chatbar/index.jsx
@@ -0,0 +1,18 @@
+import { React, useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+
+function Chatbar() {
+  return (
+    <>
+      <img
+        src="https://image5jvqbd.fmkorea.com/files/attach/new/20210108/5665468/48754678/3313916212/99b983892094b5c6d2fc3736e15da7d1.png"
+        alt=""
+      />
+      <div>개발자</div>
+      <div>have a nice day!</div>
+    </>
+  );
+}
+
+export default Chatbar;
